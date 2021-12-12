5f65f5ac8276bfb2f7f1bfdd8b6869e0729a175e
BuildTools <unconfigured@null.spigotmc.org>
2021-12-12 20:42:17 +0100

    Update Weekend 2

--- Code/functions.js
+++ Code/functions.js
@@ -1,24 +1,3 @@
-function responsiv_navigation() {
-    var x = document.getElementById("navigationId");
-    if (x.className === "navigation") {
-      x.className += " responsive";
-    }
-    else {
-      x.className = "navigation";
-    }
-  }
-
-  var navMobile = document.getElementById("navMobile");
-
-  navMobile.style.maxHeight = "0px";
-
-  function toggle(){
-      if(navMobile.style.maxHeight == "0px")
-      {
-        navMobile.style.maxHeight == "100px"
-      }
-      else
-      {
-        navMobile.style.maxHeight == "0px"
-      }
-  }
+/*
+JS-Script
+*/
